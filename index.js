4de8808908751e29ca1cd79651c55798022632b1
aligolji <aligolji@users.noreply.github.com>
2020-09-05 00:31:54 -0700

    api mvp complete. need interview questions. deploying

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+require('dotenv').config();
+
 const server = require ('./server');
 
-const port = 5000
-server.listen(port, () => console.log(`Server listening on ${port}.`));
+const port = process.env.PORT;
+
+// const port = 5000
+server.listen(port, () => console.log(`Server listening on http://localhost:${port}.`));
